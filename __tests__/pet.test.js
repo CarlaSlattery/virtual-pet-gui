0875167eea5508cc50015027952c0f5332f23a15
CarlaSlattery <cslatterydev@gmail.com>
2022-10-19 14:08:40 +0100

    added hunger and fitness properties and tests

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,9 +4,6 @@ describe("constructor", () => {
   it("returns an object", () => {
     expect(new Pet("Fido")).toBeInstanceOf(Object);
   });
-});
-
-describe("constructor", () => {
   it("sets the name property", () => {
     const pet = new Pet("Fido");
     expect(pet.name).toEqual("Fido");
@@ -15,6 +12,14 @@ describe("constructor", () => {
     const pet = new Pet("Fido");
     expect(pet.age).toEqual(0);
   });
+  it("has an initial hunger of 0", () => {
+    const pet = new Pet("Fido");
+    expect(pet.hunger).toEqual(0);
+  });
+  it("has an initial fitness of 10", () => {
+    const pet = new Pet("Fido");
+    expect(pet.fitness).toEqual(10);
+  });
 });
 
 describe("growUp", () => {
@@ -22,5 +27,15 @@ describe("growUp", () => {
     const pet = new Pet("Fido");
     pet.growUp();
     expect(pet.age).toEqual(1);
+  });
+  it("increments hunger by 5 for each increase in age of 1", () => {
+    const pet = new Pet("Fido");
+    pet.growUp();
+    expect(pet.hunger).toEqual(pet.age * 5);
+  });
+  it("decreases fitness by 3 with each increase in age of 1", () => {
+    const pet = new Pet("Fido");
+    pet.growUp();
+    expect(pet.fitness).toEqual(pet.age * 3 + pet.fitness);
   });
 });
